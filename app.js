a07931a81ecf3a84a672ced059a573f0ad0ec223
ayush109-2528 <77316982+ayush109-2528@users.noreply.github.com>
2022-11-25 10:12:04 +0530

    api update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "your api key";
+let api_key = "AIzaSyCMCpIU81apiiIH6Z0lMMOPPvn89Yi3gdE";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
